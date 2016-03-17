709f76c2887664cb804d2c6034f589a9eefa2c28
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-03-17 01:01:54 -0400

    1st change
    1st edit

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -30,4 +30,43 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             #       3. place the randomly-generated quizzes in the 'quizzes' directory.
             #       4. plaec the corresponding answers in the 'answers' directory.
             
-            
+            quizpath = "./quizzes"
+anspath = "./answers"
+os.makedirs(quizpath, exist_ok =True)
+os.makedirs(anspath, exist_ok=True)
+
+for quizNum in range(5):
+   quizFile = open('./quizzes/capitalsquiz%s.txt' % (quizNum + 1), 'w') 
+   anskeyFile =  open('./answers/capitalsquiz_answers%s.txt' % (quizNum+1), 'w')
+
+
+   quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+   quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+   quizFile.write('\n\n')
+
+
+   states = list(capitals.keys())
+   random.shuffle(states)
+
+
+   for questionNum in range(50):
+       correctAnswer = capitals[states[questionNum]]
+       wrongAnswers = list(capitals.values())
+       del wrongAnswers[wrongAnswers.index(correctAnswer)]
+       wrongAnswers = random.sample(wrongAnswers, 3)
+       answerOptions= wrongAnswers + [correctAnswer]
+       random.shuffle(answerOptions)
+
+       quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+
+       for i in range(4):
+           quizFile.write(' %s. %s\n' %('ABCD'[i], answerOptions[i]))
+           quizFile.write('\n')
+
+
+       anskeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+
+   print('A random quiz and answer key has been generated!')
+
+quizFile.close()
+anskeyFile.close()
